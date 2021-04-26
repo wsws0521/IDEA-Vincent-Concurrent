256588a209dbe9748ee32cb81e0390b54aeb5fbe
VincentLaptop <623540439@qq.com>
2021-04-26 16:32:32 +0800

    线程池试试

---
+++ TestByHand/src/cn/vincent/threadpool/Test1.java
@@ -0,0 +1,9 @@
+package cn.vincent.threadpool;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class Test1 {
+    public static void main(String[] args) {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor();
+    }
+}
